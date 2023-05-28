dc1df635ed26d178ce9a8216589fc4b7642b1d2b
taozhen <taoz@gantsoftware.com>
2023-05-28 19:41:37 +0800

    1

--- magicbox-backend/magicbox-app/magicbox-app-card/src/main/java/com/manmande/magicbox/app/card/controller/ApplyController.java
+++
@@ -1,4 +0,0 @@
-package com.manmande.magicbox.app.card.controller;
-
-public class ApplyController {
-}
--- magicbox-backend/magicbox-app/magicbox-app-card/src/main/java/com/manmande/magicbox/app/card/mapper/TlCardEventLogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.manmande.magicbox.app.card.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.manmande.magicbox.app.card.po.TlCardEventLog;
-
-public interface TlCardEventLogMapper extends BaseMapper<TlCardEventLog> {
-}
